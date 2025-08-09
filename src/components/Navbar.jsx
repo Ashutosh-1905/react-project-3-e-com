a932c94ac6ab7bfbc6ae87b044a76782ddc80cc2
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-09 17:56:52 +0530

    some changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <nav className="w-[15%] min-h-screen bg-zinc-100 flex flex-col items-center p-5">
-      {/* Add Product Button */}
+    <nav className="w-60 min-h-screen bg-zinc-100 flex flex-col items-center p-5">
       <NavLink
         to="/create"
         className="w-full text-center py-2 px-4 mb-4 border rounded border-blue-200 text-blue-500 bg-white shadow hover:bg-blue-50 transition"
@@ -14,20 +13,23 @@ const Navbar = () => {
 
       <hr className="w-full my-2" />
 
-      {/* Navigation Links */}
       <NavLink to="/" className="text-xl font-bold mb-3">
         MyStore
       </NavLink>
-      <NavLink to="/" className="hover:underline mb-2">
-        Home
-      </NavLink>
-      <NavLink to="/cart" className="hover:underline mb-4">
-        Cart
-      </NavLink>
 
-      {/* Category Filter */}
-      <h1 className="text-lg font-semibold mb-3 w-[80%]">Category Filter</h1>
-      <ul className="w-[80%] p-3 rounded-lg bg-white shadow-sm">
+      <div className="flex flex-col items-start w-full px-4 space-y-2 mb-4">
+        <NavLink to="/" className="hover:underline">
+          Home
+        </NavLink>
+        <NavLink to="/cart" className="hover:underline">
+          Cart
+        </NavLink>
+      </div>
+
+      <h1 className="text-lg font-semibold mb-3 w-full pl-4">
+        Category Filter
+      </h1>
+      <ul className="w-[85%] p-3 rounded-lg bg-white shadow-sm">
         <li className="flex items-center mb-3 cursor-pointer hover:opacity-80">
           <span className="rounded-full w-4 h-4 bg-blue-300 mr-2"></span> Cart-1
         </li>
