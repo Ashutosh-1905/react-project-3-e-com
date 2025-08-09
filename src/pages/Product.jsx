655e2dd8318a2a95c160a728dff94175bbf216cd
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-09 13:48:31 +0530

    single product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../services/api';
+import { useParams } from 'react-router';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const {id}  = useParams();
+
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+
+                const response = await api.get(`/products/${id}`);
+                setProduct(response.data)
+            } catch {
+                setProduct(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProduct();
+    }, [id])
+ return (
+   <div className="bg-white p-6 rounded-lg shadow max-w-4xl mx-auto">
+     <div className="flex flex-col md:flex-row gap-6">
+       <img
+         src={product.image}
+         alt={product.title}
+         className="w-full md:w-1/3 h-80 object-contain"
+       />
+       <div className="flex-1">
+         <h1 className="text-2xl font-bold">{product.title}</h1>
+         <p className="text-gray-600 mt-1">{product.category}</p>
+         <div className="text-2xl font-semibold text-green-600 mt-4">
+           ${product.price}
+         </div>
+         <p className="mt-4 text-gray-700">{product.description}</p>
+
+         <button className="mt-6 bg-blue-600 text-white px-4 py-2 rounded">
+           Add to cart
+         </button>
+       </div>
+     </div>
+   </div>
+ );
 }
 
 export default Product
